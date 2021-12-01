50ddc99ae8e2ef6bff5d5b0cb41448681e9e777c
xup61 <xup61i6u6@gmail.com>
2021-12-02 05:49:57 +0800

    prepare to change the coding way

--- main.py
+++ main.py
@@ -14,8 +14,8 @@ class Text_box:
 
     def draw(self, surface):
         action = False
-        color = (100, 100, 200)
         text = ""
+        color = (100, 100, 200)
         # get mouse position
         pos = pygame.mouse.get_pos()
         # check mouseover and clicked conditions
@@ -24,9 +24,16 @@ class Text_box:
                 action = True
                 self.clicked = True
                 color = Text_box.color_active if action else Text_box.color_inactive
-                print(color)
-        if pygame.mouse.get_pressed()[0] == 0:
-            self.clicked = False
+                pygame.time.wait(100)
+            elif pygame.mouse.get_pressed()[0] == 1 and self.clicked == True:
+                action = False
+                self.clicked = False
+                color = Text_box.color_inactive
+                pygame.time.wait(100)
+        if pygame.mouse.get_pressed()[0] == 0 and self.clicked == True:
+            action = True
+            self.clicked = True
+            color = Text_box.color_active if action else Text_box.color_inactive
 
         text_surface = self.font.render(text, True, color)
         input_box_width = max(200, text_surface.get_width() + 10)
@@ -35,7 +42,7 @@ class Text_box:
         surface.blit(text_surface, (self.input_box.x + 5, self.input_box.y + 5))
         pygame.draw.rect(surface, color, self.input_box, 3)
 
-        return action
+        return text
 
 
 def main():
@@ -44,6 +51,7 @@ def main():
     height = 300
     active = False
     running = True
+    text = ""
 
     # Init
     pygame.init()
@@ -52,9 +60,9 @@ def main():
     screen = pygame.display.set_mode((width, height))
 
     # Run
-    speed_text_box = Text_box(300, 150)
+    speed_text_box = Text_box(200, 100)
+    speed_text_box.draw(screen)
     while running:
-        speed_text_box.draw(screen)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
